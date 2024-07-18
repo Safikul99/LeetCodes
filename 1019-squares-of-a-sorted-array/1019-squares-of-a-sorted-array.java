8ac2abfab35beffc8a952762895809c5ebd3522b
Safikul Islam <126138940+Safikul99@users.noreply.github.com>
2024-07-18 21:58:18 +0530

    Time: 1 ms (100%), Space: 47.5 MB (6.68%) - LeetHub

--- 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.java
+++ 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.java
@@ -1,32 +1,39 @@
 class Solution {
     public int[] sortedSquares(int[] nums) 
     {
-        for(int i=0;i<nums.length;i++)
+        int n=nums.length;
+        int i=0;
+        for(i=0;i<n;i++)
         {
-            nums[i]=nums[i]*nums[i];
-        }
-        for(int i=0;i<nums.length;i++)
-        {
-             boolean b=false;
-            for(int j=0;j<nums.length-1;j++)
-            {
-               
-                if(nums[j]>nums[j+1])
-                {
-                    b=true;
-
-                    int temp=nums[j+1];
-                    nums[j+1]=nums[j];
-                    nums[j]=temp;
-
-                }
-            }
-            if(b==false)
+            if(nums[i]>=0)
             {
                 break;
             }
         }
-        return nums;
-        
+        for(int j=0;j<n;j++)
+        {
+            nums[j]=nums[j]*nums[j];
+        }
+        int j=i-1,k=0;
+        int []res=new int[n];
+        while(j>=0&& i<n)
+        {
+            if(nums[i]<nums[j])
+            {
+                res[k++]=nums[i++];
+            }
+            else
+            res[k++]=nums[j--];
+        }
+        while(i<n)
+        {
+            res[k++]=nums[i++];
+        }
+        while(j>=0)
+        {
+            res[k++]=nums[j--];
+        }
+        return res;
     }
-}
+       
+        }
