281523b01218a536683eb0e5c772fdc6a417b389
Safikul Islam <126138940+Safikul99@users.noreply.github.com>
2024-07-18 21:16:06 +0530

    Time: 437 ms (5.01%), Space: 46.3 MB (80.66%) - LeetHub

--- 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.java
+++ 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.java
@@ -7,15 +7,23 @@ class Solution {
         }
         for(int i=0;i<nums.length;i++)
         {
+             boolean b=false;
             for(int j=0;j<nums.length-1;j++)
             {
+               
                 if(nums[j]>nums[j+1])
                 {
+                    b=true;
+
                     int temp=nums[j+1];
                     nums[j+1]=nums[j];
                     nums[j]=temp;
 
                 }
+            }
+            if(b==false)
+            {
+                break;
             }
         }
         return nums;
