36a5ff8b90d72b48c5f7d6b523af5550ead18d66
Safikul Islam <126138940+Safikul99@users.noreply.github.com>
2024-07-26 15:57:57 +0530

    Time: 0 ms (100%), Space: 45.5 MB (78.07%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void reverseString(char[] s) 
+    {
+        int n=s.length;
+        int st=0,e=n-1;
+        while(st<=e)
+        {
+            char temp=s[st];
+            s[st]=s[e];
+            s[e]=temp;
+            st++;
+            e--;
+        }
+        
+    }
+}
